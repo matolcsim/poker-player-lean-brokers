0f7527881ba98c14e07f3977ad4b615244285316
balazske96 <venyigebalazs96@gmail.com>
2019-05-27 11:48:11 +0200

    first commit / pair comparison!

--- player.py
+++ player.py
@@ -1,10 +1,21 @@
 
 class Player:
-    VERSION = "v0.1"
+    VERSION = "v1.0"
 
     def betRequest(self, game_state):
+
+        players = game_state['players']
+        pot = game_state['pot']
+        big_blind = game_state['pot']
+        bet = max(pot, big_blind)
+        for player in players:
+            if player['name'] == "Lean Brokers":
+                if player['hole_cards'][0]['rank'] == player['hole_cards'][1]['rank']:
+                    return bet
+
         return 0
 
     def showdown(self, game_state):
+        print(game_state)
         pass
 
