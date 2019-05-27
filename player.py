a1b6a8e38698bf522a05fc09916df339a1ad7a73
balazske96 <venyigebalazs96@gmail.com>
2019-05-27 14:29:12 +0200

    pair and drill sensitive game

--- player.py
+++ player.py
@@ -1,10 +1,10 @@
+
 class Player:
     VERSION = "v1.0"
 
     def betRequest(self, game_state):
 
         players = game_state['players']
-
 
         for player in players:
             # PLAYER CARDS
@@ -25,13 +25,88 @@ class Player:
 
             # 'MAIN'
             if player['name'] == "Lean Brokers":
-                if player['hole_cards'][0]['rank'] == player['hole_cards'][1]['rank']:
-                    if player['hole_cards'][0]['rank'] in ['K', 'A', 'Q', 'J']:
-                        return player['stack']
-                    return bet
 
         return 0
 
     def showdown(self, game_state):
         pass
+
+    def pair(self, card1_rank, card2_rank, community):
+
+        # RETURN OPTIONS (string):
+        # "big_pair"
+        # "little_pair"
+        # "two_pair"
+        # "two_pair_one_big"
+        # "two_pair_two_big"
+
+        if card1_rank == card2_rank:
+            if card1_rank in ['K', 'A', 'Q', 'J']:
+                return "big_pair"
+            else:
+                return "little_pair"
+        else:
+            true_pair_1 = 0
+            true_pair_2 = 0
+            for card in community:
+                if card[0] == card1_rank:
+                    true_pair_1 += 1
+                elif card[0] == card2_rank:
+                    true_pair_2 += 1
+            if true_pair_1 == 1 and true_pair_2 != 1:
+                if card1_rank in ['K', 'A', 'Q', 'J']:
+                    return "big_pair"
+                else:
+                    return 'little_pair'
+            elif true_pair_2 == 1 and true_pair_1 != 1:
+                if card2_rank in ['K', 'A', 'Q', 'J']:
+                    return "big_pair"
+                else:
+                    return 'little_pair'
+            elif true_pair_1 == 1 and true_pair_2 == 1:
+                if card1_rank in ['K', 'A', 'Q', 'J'] and card2_rank not in ['K', 'A', 'Q', 'J']:
+                    return "two_pair_one_big"
+                elif card1_rank not in ['K', 'A', 'Q', 'J'] and card2_rank  in ['K', 'A', 'Q', 'J']:
+                    return "two_pair_one_big"
+                elif card1_rank in ['K', 'A', 'Q', 'J'] and card2_rank  in ['K', 'A', 'Q', 'J']:
+                    return "two_pair_two_big"
+                else:
+                    return "two_par"
+
+    def drill(self, card1_rank, card2_rank, community):
+
+        # RETURN OPTIONS (string):
+        # "big_drill"
+        # "little_drill"
+
+        if card1_rank == card2_rank:
+            true_drill = 0
+            for card in community:
+                if card[0] == card1_rank:
+                    true_drill += 1
+            if true_drill == 1:
+                if card1_rank in ['K', 'A', 'Q', 'J']:
+                    return "big_drill"
+                else:
+                    return "little_drill"
+        else:
+            true_drill_1 = 0
+            true_drill_2 = 0
+            for card in community:
+                if card[0] == card1_rank:
+                    true_drill_1 += 1
+                elif card[0] == card2_rank:
+                    true_drill_2 += 1
+            if true_drill_1 == 2:
+                if card1_rank in ['K', 'A', 'Q', 'J']:
+                    return "big_drill"
+                else:
+                    return "little_drill"
+            elif true_drill_2 == 2:
+                if card2_rank in ['K', 'A', 'Q', 'J']:
+                    return "big_drill"
+                else:
+                    return "little_drill"
+
+
 
